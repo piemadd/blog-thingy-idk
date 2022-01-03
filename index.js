3dcdbba7f6e79f3099a6e5ba194a329236a703d5
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2022-01-03 04:05:35 +0000

    Simpler index.js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 //realized that instead of having a "static" folder and having my build script copy stuff over, i can just have ejs file(s) for scripts and styles
-
 const express = require('express');
-const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-});
-
-app.get("/posts/:path", (req, res) => {
-    let filePath = req.params.path
-
-    if (!filePath.endsWith('.html')) {
-        filePath += '.html'
-    }
-    
-    res.sendFile(path.join(__dirname, "dist", "posts", filePath));
-});
+app.use('/', express.static('dist'));
 
 app.listen(3000, () => {
     console.log('server started');
